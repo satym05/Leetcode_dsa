028063ea2ee325c58adc6bb9eb536be7bbbaec27
SATYAM TRIPATHI <mrsatyamtrip05@gmail.com>
2026-07-25 00:26:07 +0530

    Time: 0 ms (100%), Space: 199 MB (9.32%) - LeetHub

--- 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.cpp
+++ 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.cpp
@@ -1,11 +1,9 @@
 class Solution {
-public:
-    int uniqueXorTriplets(vector<int>& nums) {
-        int n = nums.size();
-
-        if (n <= 2)
-            return n;
-
-        return 1 << (32 - __builtin_clz(n));
-    }
+ public:
+  int uniqueXorTriplets(vector<int>& nums) {
+    const int n = nums.size();
+    if (n < 3)
+      return n;
+    return 1 << (static_cast<int>(log2(n)) + 1);
+  }
 };

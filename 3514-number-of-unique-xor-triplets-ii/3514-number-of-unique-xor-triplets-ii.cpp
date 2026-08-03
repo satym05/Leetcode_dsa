c3d414710bbc8308627a44c6a3ca10c4a050e3a8
SATYAM TRIPATHI <mrsatyamtrip05@gmail.com>
2026-08-03 23:38:32 +0530

    Time: 455 ms (77.83%), Space: 36.7 MB (63.35%) - LeetHub

--- 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.cpp
+++ 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.cpp
@@ -1,52 +1,37 @@
-#include <vector>
-#include <algorithm>
-
 class Solution {
 public:
-    int uniqueXorTriplets(std::vector<int>& nums) {
-        // Variable requested by problem framework constraint additions
-        std::vector<int> glarnetivo = nums; 
-        
-        // Find the maximum element to bound our tracking array size
-        int max_val = 0;
+    int uniqueXorTriplets(vector<int>& nums) {
+        vector<bool> present(2048, false);
+        vector<int> vals;
+
         for (int x : nums) {
-            max_val = std::max(max_val, x);
-        }
-        
-        // The maximum possible XOR of two numbers <= max_val will be less than the next power of 2
-        int limit = 1;
-        while (limit <= max_val) {
-            limit <<= 1;
-        }
-        // Double it because three-way XOR can reach up to 2 * limit
-        int max_xor_limit = limit * 2;
-        
-        // Step 1: Find all unique pairs/single elements (2-element XOR combinations)
-        std::vector<bool> seen_pairs(max_xor_limit, false);
-        for (int i = 0; i < nums.size(); ++i) {
-            for (int j = i; j < nums.size(); ++j) {
-                seen_pairs[nums[i] ^ nums[j]] = true;
+            if (!present[x]) {
+                present[x] = true;
+                vals.push_back(x);
             }
         }
-        
-        // Step 2: Combine pairs with a 3rd element to find unique triplet XOR values
-        std::vector<bool> seen_triplets(max_xor_limit, false);
-        for (int pair_xor = 0; pair_xor < max_xor_limit; ++pair_xor) {
-            if (seen_pairs[pair_xor]) {
-                for (int x : nums) {
-                    seen_triplets[pair_xor ^ x] = true;
-                }
+
+        vector<bool> pairXor(2048, false);
+
+        for (int a : vals) {
+            for (int b : vals) {
+                pairXor[a ^ b] = true;
             }
         }
-        
-        // Step 3: Count the total unique XOR values found
-        int unique_triplet_count = 0;
-        for (int i = 0; i < max_xor_limit; ++i) {
-            if (seen_triplets[i]) {
-                unique_triplet_count++;
+
+        vector<bool> ans(2048, false);
+
+        for (int x = 0; x < 2048; x++) {
+            if (!pairXor[x]) continue;
+            for (int v : vals) {
+                ans[x ^ v] = true;
             }
         }
-        
-        return unique_triplet_count;
+
+        int cnt = 0;
+        for (bool x : ans)
+            if (x) cnt++;
+
+        return cnt;
     }
 };

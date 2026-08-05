c473cfb41281a77a78257a444e16f2c541bb67ce
SATYAM TRIPATHI <mrsatyamtrip05@gmail.com>
2026-08-05 23:07:58 +0530

    Time: 185 ms (61.38%), Space: 308.2 MB (62.41%) - LeetHub

---
+++ 3310-remove-methods-from-project/3310-remove-methods-from-project.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    void dfs(int u, vector<vector<int>>& adj, vector<int>& vis) {
+        vis[u] = 1;
+        for (int v : adj[u]) {
+            if (!vis[v])
+                dfs(v, adj, vis);
+        }
+    }
+
+    vector<int> remainingMethods(int n, int k, vector<vector<int>>& invocations) {
+        vector<vector<int>> adj(n);
+
+        for (auto &e : invocations)
+            adj[e[0]].push_back(e[1]);
+
+        vector<int> suspicious(n, 0);
+        dfs(k, adj, suspicious);
+
+        // Check if any outside method invokes a suspicious one
+        for (auto &e : invocations) {
+            int u = e[0], v = e[1];
+            if (!suspicious[u] && suspicious[v]) {
+                vector<int> ans;
+                for (int i = 0; i < n; i++)
+                    ans.push_back(i);
+                return ans;
+            }
+        }
+
+        vector<int> ans;
+        for (int i = 0; i < n; i++)
+            if (!suspicious[i])
+                ans.push_back(i);
+
+        return ans;
+    }
+};
